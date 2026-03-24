bd5cc75c0a0361f67d11814ad38b7f6152ec4c4c
JohNNy <johnnymundi@hotmail.com>
2026-03-24 14:11:37 -0300

    feat: add simple health check

--- licensing/urls.py
+++ licensing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import activate, validate_license
+from .views import activate, validate_license, health
 
 urlpatterns = [
     path("activate/", activate),
     path("validate-license/", validate_license),
+    path("health", health),
 ]
